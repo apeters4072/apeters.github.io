73419248c9c06b629b2633d8800c1107709a5836
apeters4072 <apeters4072@gmail.com>
2026-05-12 17:23:17 -0400

    Added 'The Himalayas' project

--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@ const projects = [
   {
     title: "2025 Mapping Challenge",
     category: ["carto"],
-    description: "A series of maps created during the #30DayMapChallenge.",
+    description: "A series of maps created during the #30DayMapChallenge",
     image: "assets/projects/2025 Mapping Challenge/2025 Mapping Challenge_Day 2_Lines.png",
     projectLink: "assets/projects/2025 Mapping Challenge/2025_MappingChallenge.html",
     // githubLink: "https://github.com/apeters4072/project",
@@ -11,4 +11,16 @@ const projects = [
     year: 2025
   },
 
+  {
+    title: "The Himalayas",
+    category: ["carto"],
+    description: "Depiciting the Highest Peaks in the World",
+    image: "assets/projects/The Himalayas/The Himalayas.png",
+    projectLink: "assets/projects/The Himalayas/The_Himalayas.html",
+    // githubLink: "https://github.com/apeters4072/project",
+    tags: ["Cartography"],
+    featured: false,
+    year: 2024
+  },
+  
 ];
