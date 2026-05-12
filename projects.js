3ea54464da7d2c36d28b0d9b98e388f64096c7ec
apeters4072 <apeters4072@gmail.com>
2026-05-12 16:47:47 -0400

    Initialized projects.js

---
+++ projects.js
@@ -0,0 +1,18 @@
+const projects = [
+  {
+    title: "2025 Mapping Challenge",
+    category: ["carto"],
+    description: "A series of maps created during the #30DayMapChallenge.",
+    image: "assets/projects/2025 Mapping Challenge/2025 Mapping Challenge_Day 2_Lines.png",
+    projectLink: "assets/projects/2025 Mapping Challenge/2025_MappingChallenge.html",
+    // githubLink: "https://github.com/apeters4072/project",
+    technologies: [
+      "ArcGIS Pro",
+      "Cartography",
+      "Design"
+    ],
+    featured: true,
+    year: 2025
+  },
+
+];
