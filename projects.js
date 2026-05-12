277db526f1beeb1f12fc673fdf7f3cf1a7a88d15
apeters4072 <apeters4072@gmail.com>
2026-05-12 17:18:51 -0400

    Replace technologies with tags in project entry
    Removed technologies array and replaced it with tags.

--- projects.js
+++ projects.js
@@ -6,11 +6,7 @@ const projects = [
     image: "assets/projects/2025 Mapping Challenge/2025 Mapping Challenge_Day 2_Lines.png",
     projectLink: "assets/projects/2025 Mapping Challenge/2025_MappingChallenge.html",
     // githubLink: "https://github.com/apeters4072/project",
-    technologies: [
-      "ArcGIS Pro",
-      "Cartography",
-      "Design"
-    ],
+    tags: ["Cartography"],
     featured: true,
     year: 2025
   },
