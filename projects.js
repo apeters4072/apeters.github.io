cfcecddd7749a5f4ffb5201486057c50f7656afe
apeters4072 <apeters4072@gmail.com>
2026-05-12 20:51:43 -0400

    Refactor projects array and update method property

--- projects.js
+++ projects.js
@@ -1,59 +1,3 @@
-const projects = [
-  {
-    title: "2025 Mapping Challenge",
-    category: ["carto"],
-    description: "A series of maps created during the #30DayMapChallenge",
-    image: "assets/projects/2025 Mapping Challenge/2025 Mapping Challenge_Day 2_Lines.png",
-    projectLink: "assets/projects/2025 Mapping Challenge/2025_MappingChallenge.html",
-    // githubLink: "https://github.com/apeters4072/project",
-    tags: [
-      "ArcGIS Pro",
-      "Cartography"
-    ],
-    featured: true,
-    year: 2025
-  },
-
-  {
-    title: "The Himalayas",
-    category: [
-      "carto",
-      "school"],
-    description: "Depiciting the Highest Peaks in the World",
-    image: "assets/projects/The Himalayas/The Himalayas.png",
-    projectLink: "assets/projects/The Himalayas/The_Himalayas.html",
-    // githubLink: "https://github.com/apeters4072/project",
-    tags: [
-      "ArcGIS Pro",
-      "Cartography",
-      "School"
-    ],
-    featured: false,
-    year: 2024
-  },
-  
-  {
-    title: "SVI Trends and Slave Populations",
-    category: [
-      "carto",
-      "school"
-    ],
-    description: "Exploring long-term trends of historical slave populations",
-    image: "assets/projects/SVI Trends and Slave Populations/SVI Trends Amongst Slave Populations.png",
-    projectLink: "assets/projects/SVI Trends and Slave Populations/SVI_Trends_and_Slave_Populations.html",
-    // githubLink: "https://github.com/apeters4072/project",
-    tags: [
-      "ArcGIS Pro",
-      "Social Vulnerability",
-      "Georectification",
-      "School"
-    ],
-    featured: false,
-    year: 2025
-  },
-  
-];
-
 // To change from a static image to an AGOL webmap, change 'type: "image"' to 'type: "agol"'
 // Additionally, change 'src:' to 'itemId:' and input web map itemID
 
@@ -126,7 +70,7 @@ const projects = [
       },
     ],
 
-    methods: null,
+    method: null,
     
     sources: [
       "OSM",
